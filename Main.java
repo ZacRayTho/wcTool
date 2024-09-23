0f63c433c90ffd12c59e1d6d3209a78ad8778b5c
zacraytho <zacraytho@gmail.com>
2024-09-22 22:27:33 -0400

    working on step one, reading file

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        String file = String.format("./%s", args[args.length - 1]);
+        // How to Open/read file
+
+        switch(args[0]) {
+            case "-c": 
+                // How to read bytes in a file
+                break;
+        }
+        // System.out.println(file);
+    }
+}
