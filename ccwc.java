8f7804ede7b57b95718c67dacf479d1c300f777b
zacraytho <zacraytho@gmail.com>
2024-09-25 20:29:50 -0400

    working on step 4, counting characters

--- ccwc.java
+++ ccwc.java
@@ -1,4 +1,6 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class ccwc {
     public static void main(String[] args) {
@@ -9,7 +11,58 @@ public class ccwc {
                 System.out.println(String.format("%d %s", txt.length(), txt.getName()));
                 break;
             case "-l":
-                // Read number of lines in a file
+                int lineCount = 0;
+
+                try {
+                    Scanner reader = new Scanner(txt);
+                    while (reader.hasNextLine()) {
+                        reader.nextLine();
+                        lineCount += 1;
+                    }
+                    reader.close();
+                } 
+                catch (FileNotFoundException e) {
+                    System.out.println("An error occurred.");
+                    e.printStackTrace();
+                }
+
+                System.out.println(String.format("%d %s", lineCount, txt.getName()));
+                break;
+            case "-w":
+                int wordCount = 0;
+
+                try {
+                    Scanner reader = new Scanner(txt);
+                    while (reader.hasNext()) {
+                        reader.next();
+                        wordCount += 1;
+                    }
+                    reader.close();
+                } 
+                catch (FileNotFoundException e) {
+                    System.out.println("An error occurred.");
+                    e.printStackTrace();
+                }
+
+                System.out.println(String.format("%d %s", wordCount, txt.getName()));
+                break;
+            case "-m":
+                int characterCount = 0;
+
+                try {
+                    Scanner reader = new Scanner(txt);
+                    while (reader.hasNextLine()) {
+                        
+                        characterCount += reader.nextLine().length();
+                    }
+                    reader.close();
+                } 
+                catch (FileNotFoundException e) {
+                    System.out.println("An error occurred.");
+                    e.printStackTrace();
+                }
+
+                System.out.println(String.format("%d %s", characterCount, txt.getName()));
                 break;
         }
        
